f7afe13c48eb424bc145012e4d4a93ab44acf624
mehedi825 <taninmehedi5@gmail.com>
2025-07-05 14:18:56 +0600

    Create src/component/home.jsx

---
+++ clien/src/component/home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section className="h-screen bg-red-600 text-white flex items-center justify-center">
+      <h1 className="text-4xl md:text-6xl font-bold">Benvenuto! Italian Delight</h1>
+    </section>
+  );
+}
